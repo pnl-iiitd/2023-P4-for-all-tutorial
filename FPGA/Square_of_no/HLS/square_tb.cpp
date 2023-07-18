9461facd2c8bf2a96198c098e0bbbdfda8de0676
Neeraj Kumar Yadav <101931370+neerajy1998@users.noreply.github.com>
2023-07-18 16:36:21 +0530

    square_tb.cpp

--- FPGA/Square_of_no/HLS/square_tb.cpp
+++ FPGA/Square_of_no/HLS/square_tb.cpp
@@ -1,24 +1,54 @@
-#include"head.h"
+#include<stdio.h>
+#include "head.h"
 
-void square_hw(hls::stream<axis_data> &input, hls::stream<axis_data> & output)
+void square_hw(hls::stream<axis_data> &input, hls::stream<axis_data> & output);
+void square_sw(int input[SIZE], int output[SIZE])
 {
-#pragma HLS INTERFACE axis register both port=output
-#pragma HLS INTERFACE axis register both port=input
-#pragma HLS INTERFACE ap_ctrl_none port=return
-
-	axis_data local_read,local_write;
 	for(int i=0;i<SIZE;i++)
 	{
-		local_read=input.read();
-		local_write.data=(local_read.data)*(local_read.data);
+		output[i]=input[i]*input[i];
+	}
+}
+
+int main()
+{
+	int input[SIZE],output_sw[SIZE],output_hw[SIZE];
+	for(int i=0;i<SIZE;i++)
+	{
+		input[i]=i;
+	}
+//	square_hw(input,output_hw);
+	square_sw(input,output_sw);
+
+	hls::stream<axis_data> in_vec,out_vec;
+	axis_data local_read,local_write;
+
+	for(int i=0;i<SIZE;i++)
+	{
+		local_read.data=input[i];
 		if(i==SIZE-1)
 		{
-			local_write.last=1;
+			local_read.last=1;
 		}
 		else
 		{
-			local_write.last=0;
+			local_read.last=0;
 		}
-		output.write(local_write);
+		in_vec.write(local_read);
 	}
+	square_hw(in_vec,out_vec);
+
+
+	for(int i=0;i<SIZE;i++)
+	{
+		local_write=out_vec.read();
+		if(output_sw[i]!=local_write.data)
+		{
+			printf("Error at %d",i);
+			return 1;
+		}
+	}
+	printf("No error\n");
+	return 0;
+
 }
