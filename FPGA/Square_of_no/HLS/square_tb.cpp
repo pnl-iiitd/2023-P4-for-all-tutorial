dda15cd617613603a260b9cd821e91e63093b546
Neeraj Kumar Yadav <101931370+neerajy1998@users.noreply.github.com>
2023-07-18 16:35:53 +0530

    square_tb.cpp

--- FPGA/Square_of_no/HLS/square_tb.cpp
+++ FPGA/Square_of_no/HLS/square_tb.cpp
@@ -1,1 +1,24 @@
+#include"head.h"
 
+void square_hw(hls::stream<axis_data> &input, hls::stream<axis_data> & output)
+{
+#pragma HLS INTERFACE axis register both port=output
+#pragma HLS INTERFACE axis register both port=input
+#pragma HLS INTERFACE ap_ctrl_none port=return
+
+	axis_data local_read,local_write;
+	for(int i=0;i<SIZE;i++)
+	{
+		local_read=input.read();
+		local_write.data=(local_read.data)*(local_read.data);
+		if(i==SIZE-1)
+		{
+			local_write.last=1;
+		}
+		else
+		{
+			local_write.last=0;
+		}
+		output.write(local_write);
+	}
+}
